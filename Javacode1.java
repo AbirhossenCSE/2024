5c4ad227e20299ce1c04aa5b44b984bf9883b106
Abir Hossen <99603168+AbirhossenCSE@users.noreply.github.com>
2024-05-16 12:01:58 +0600

    java

---
+++ Javacode1.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javacode1;
+
+/**
+ *
+ * @author Pc
+ */
+public class Javacode1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+//        int num;
+//        double d;
+//        num = 10;
+//        d = 3.33;
+//        
+//        System.out.println("Hello World: "+ num + " " + d);
+//        
+//        
+//        System.out.println("");
+
+//        Scanner userInput = new Scanner(System.in); // preparing system for input
+//        int a = userInput.nextInt();
+//        System.out.println("User intput is : " + a);
+//        double b = userInput.nextDouble();
+//        System.out.println("User intput is : " + b);
+//        String c = userInput.next();
+//        System.out.println("User intput is : " + c);
+
+//        int a = 5;
+//        float b = 10;
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(a + " " + b);
+//        float c = a * b;
+//        System.out.println("c = " + c);
+
+//System.out.println("Hello World");
+//
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        System.out.println(a);
+//        String s = sc.next();
+//        System.out.println("The String is : " + s); 
+
+//        Box myBox = new Box();
+//        myBox.length = 2;
+//        myBox.height = 3;
+//        myBox.width = 4;
+//        Box largeBox = new Box();
+//        largeBox.length = 100;
+//        largeBox.width = 100;
+//        largeBox.height = 100;
+//        System.out.println("Large Box length" + largeBox.length);
+    }
+    
+}
