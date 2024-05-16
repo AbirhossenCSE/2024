5b948c2b38bfd476aa092f603a5b17949cfb51ff
Abir Hossen <99603168+AbirhossenCSE@users.noreply.github.com>
2024-05-16 12:02:18 +0600

    java

---
+++ Newcheck1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javacode1;
+
+/**
+ *
+ * @author Pc
+ */
+public class Newcheck1 {
+    public static void main(String[] args) {
+    int num1 = 10;
+    int num2 = 15;
+    int sum = num1 + num2;
+
+    System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+  }
+  }
+
